b92a06dcb03c64366a98de6d9d4a94e68fcb11c4
pedrogariglio <pedrogariglio.24@gmail.com>
2025-01-16 14:50:39 -0300

    feat: Add base template and update existing views

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de Pedidos</title>
+
+</head>
+<body>
+    <div class="cabecera">
+        <div class="titulo">
+            @yield("cabecera")
+        </div>
+    </div>
+    
+    <main>
+        @yield('content')
+    </main>
+    
+    <footer>
+        <!-- Pie de página -->
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{ProductController, OrderController};
 
+Route::resource('products', ProductController::class);
+Route::resource('orders', OrderController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
-Route::resource('orders', OrderController::class);
+
