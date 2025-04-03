fd7c83975283c0f544bc7a718e67569162e18d3c
pedrogariglio <pedrogariglio.24@gmail.com>
2025-04-03 17:23:23 -0300

    database: add suppliers table migration and apply migration

---
+++ database/migrations/2025_04_03_171349_create_suppliers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('contact_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('address');
+            $table->string('phone', 10);
+            $table->string('tax_id')->comment('RUC/CUIT/NIT')->nullable();
+            $table->text('payment_terms')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
